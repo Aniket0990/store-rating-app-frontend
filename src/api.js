8b9dec30878990a9a86961e322400624344cbee2
Aniket Shelke <aniketshelke554@gmail.com>
2025-12-04 23:54:58 +0530

    Final Commit

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
-// const API_BASE_URL = "http://10.47.60.194:8080/api";
-const API_BASE_URL = (import.meta.env.VITE_API_URL || "http://localhost:8080") + "/api";
+const API_BASE_URL = "http://10.47.60.194:8080/api";
 
 export const apiRequest = async (endpoint, method = "GET", body = null) => {
   const token = localStorage.getItem("token");
